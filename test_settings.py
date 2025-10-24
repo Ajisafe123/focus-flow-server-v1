7787a618768fe9fd35c708b6e0515865f48d1968
Ajisafe Ibrahim <ajisafeibrahim54@gmail.com>
2025-10-24 06:41:46 +0100

    Add project files

--- test_settings.py
+++
@@ -1,5 +0,0 @@
-from src.config import settings
-
-print("SECRET_KEY:", settings.SECRET_KEY)
-print("ENVIRONMENT:", settings.ENVIRONMENT)
-print("BACKEND_CORS_ORIGINS:", settings.BACKEND_CORS_ORIGINS)
