3cfdadf18db285c2988aeb10dd8f5cde6456c396
Ajisafe Ibrahim <ajisafeibrahim54@gmail.com>
2025-10-24 08:13:07 +0100

    Add project files

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://focus-flow-server-v1.onrender.com",
+        "https://focus-flow-client-v1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
